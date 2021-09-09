bf36a2efd9a7d4332728386e2eb3691b22b97d7e
Felipe Rossoni <feliperossoni05@gmail.com>
2021-09-09 16:16:32 -0300

    Create ex7.py

---
+++ ex7.py
@@ -0,0 +1,43 @@
+filmes = ['9 a salvação','A espera de um milagre','A voz do silêncio']
+print ('Filmes :', (filmes))
+jogos = ['LOL','Minecraft','Counter-Strike']
+print ('Jogos: ',(jogos))
+livros = ['Harry Potter','One Piece','A ilha do tesouro']
+print ('Livros :', (livros))
+esportes = ['Futebol','Basquete','Vôleibol']
+print ('Esportes :',(esportes))
+
+#Letra A
+filmes.append('Your Name')
+filmes.append('Extraordinário')
+print ('Filmes :', (filmes))
+jogos.append('Dota 2')
+jogos.append('Teamfight Tactics')
+print ('Jogos: ',(jogos))
+livros.append('IT')
+livros.append('Monster')
+print ('Livros :', (livros))
+esportes.append('Tênis')
+esportes.append('Tênis de mesa')
+print ('Esportes :',(esportes))
+
+#Letra B
+todas = ['Filmes','Jogos','Livros','Esportes']
+print('Todas as listas disponíveis até o momento são :',(todas))
+
+#Letra C
+#Não entendi a questão.
+
+#Letra D
+print(esportes)
+print('O Esporte Tênis será removido.')
+del esportes[3]
+print(esportes)
+
+#LetraE
+todas.insert(1,'Disciplina')
+print('Todas as listas disponíveis até o momento são :',(todas))
+
+
+
+

0abe65c7469810939f727630e4f05a25d509e23a
Felipe Rossoni <feliperossoni05@gmail.com>
2021-09-09 15:51:46 -0300

    Create ex6.py

---
+++ ex6.py
@@ -0,0 +1,4 @@
+lista = ['arroz','feijão','maçã','banana','batata','cenoura','milho','carne']
+print(lista)
+posi = input("Você deseja encontrar a posição de qual item ?")
+print("A posição de {} na lista é: ".format(posi), lista.index(posi))
